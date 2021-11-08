438b228ceff5d1c424c01cc2f2e82042c2d355d9
shadow4027 <jbloom520@gmail.com>
2021-11-08 16:44:36 -0700

    started work on pando request session

--- RabbIT.py
+++ RabbIT.py
@@ -1,3 +1,12 @@
-class Pando():
-    def __init__(self):
-        pass
+import requests
+import bs4
+
+
+class Pando:
+
+    def __init__(self, username='', password=''):
+        self.WebSession = requests.Session()
+        self.WebSession.get("")
+
+
+PanSesh = Pando("hello", "world")
